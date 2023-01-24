863442797218189587120d7009ba1b0970dd12bc
Joseph <75801386+joseph-468@users.noreply.github.com>
2023-01-24 19:57:15 +0000

    Updated fight system and reformatted code
    Fight system now has proper attacking. It clones the player instance meaning your health is reset after every fight. Eventually I aim for fights to give rewards or maybe xp and to possibly have a system where enemies can get more powerful over time. I'll need to make it so the type of enemy, it's stats and if it's unique somehow is computed and passed in when encountering enemies

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use rand::Rng;
+use std::clone::Clone;
 use std::io;
 use std::io::Write;
 use std::process::exit;
 
 // Constants
 const FLEE_CHANCE: u32 = 3;
+const ENCOUNTER_CHANCE: u32 = 3;
 const INV_SIZE: u32 = 10;
 const INV_WIDTH: u32 = 5;
 const EMPTY_STRING: String = String::new();
-const HELP_COMMAND: &str =
-    "inventory: access your inventory\nw, a, s, d: walk forwards, left, backwards or right";
+const HELP_COMMAND: &str = "Full list of commands coming later";
 
-// Useful datatypes
+// Structs
+#[derive(Clone)]
 struct Player {
     health: i32,
     defence: i32,
@@ -32,8 +34,8 @@ struct Enemy {
 }
 
 fn main() {
-    // Setup player
-    let mut player = Player {
+    // Create player instance
+    let mut player: Player = Player {
         health: 100,
         defence: 5,
         damage: 10,
@@ -46,17 +48,15 @@ fn main() {
 
     // Starting text
     clearscreen::clear().unwrap();
-    println!("Welcome!");
+    println!("Welcome! Press any key to continue");
 
     // Game loop
     loop {
-        // Get input
-        let input = get_input();
-
-        // Process input
+        // Get and process input
+        let input: String = get_input();
         match input.as_str() {
             "help" => println!("{}", HELP_COMMAND),
-            "debug" => println!("Debug\n{}", player.health), // Temporary command. Remove later
+            "debug" => println!("Debug\n{}, {}", player.health, player.weapon), // Temporary command. Remove later
             "stats" => stats(&mut player),
             "inventory" => open_inventory(&mut player),
             "w" | "a" | "s" | "d" => walk(&mut player, &input),
@@ -65,15 +65,15 @@ fn main() {
     }
 }
 
-// Functions
+// Basic input function
 fn get_input() -> String {
     // Typing prompt
     print!(">");
     io::stdout().flush().expect("Flush failed");
     // Get input
-    let mut input = String::new();
+    let mut input: String = String::new();
     io::stdin().read_line(&mut input).expect("Readline failed");
-    let input = input.trim().to_lowercase();
+    let input: String = input.trim().to_lowercase();
     // Handle exit
     if input == "exit" {
         exit(0);
@@ -84,6 +84,7 @@ fn get_input() -> String {
 }
 
 fn open_inventory(player: &mut Player) {
+    // Calculate length and number of lines
     let mut lines: u32 = INV_SIZE / INV_WIDTH;
     if INV_SIZE as f32 / INV_WIDTH as f32 % 1.0 != 0.0 {
         lines += 1;
@@ -91,7 +92,7 @@ fn open_inventory(player: &mut Player) {
 
     // Display inventory items
     for i in 0..lines {
-        let mut line = String::new();
+        let mut line: String = String::new();
         for j in 0..INV_WIDTH {
             let current_index: u32 = j + i * INV_WIDTH;
             if current_index < INV_SIZE {
@@ -110,18 +111,17 @@ fn open_inventory(player: &mut Player) {
 }
 
 fn stats(player: &mut Player) {
-    // What even is this
-    let weapon = if player.weapon == "" {
+    // Sets empty slots to None then print all stats and equipment to the screen
+    let weapon: &str = if player.weapon == "" {
         "None"
     } else {
         &player.weapon
     };
-    let armor = if player.armor == "" {
+    let armor: &str = if player.armor == "" {
         "None"
     } else {
         &player.armor
     };
-
     println!(
         "Weapon: {}, Armor: {}\nHealth: {}, Defence: {}\nDamage: {}, Speed: {}",
         weapon, armor, player.health, player.defence, player.damage, player.speed
@@ -129,71 +129,88 @@ fn stats(player: &mut Player) {
 }
 
 fn walk(player: &mut Player, direction: &str) {
-    // Definitely not the optimal way but whatever
-    let enemy_chance = rand::thread_rng().gen_range(0..1);
-    let mut enemy_found: [String; 5] = [EMPTY_STRING; 5];
-    enemy_found[0] = "and encountered an enemy".to_string();
+    // Random chance to encounter enemy
+    let enemy_found: bool = rand::thread_rng().gen_range(0..ENCOUNTER_CHANCE) == 0;
+    let mut encounter_message: String = String::new();
+    if enemy_found {
+        encounter_message += "and encountered an enemy";
+    }
+
     // Update position
     match direction {
         "w" => {
             player.position = (player.position.0, player.position.1 + 1);
-            println!("You walked forward {}", enemy_found[enemy_chance])
+            println!("You walked forward {}", encounter_message);
+            encounter_message = String::from("You walked forward and encountered an enemy");
         }
         "a" => {
             player.position = (player.position.0 - 1, player.position.1);
-            println!("You walked left {}", enemy_found[enemy_chance])
+            println!("You walked left {}", encounter_message);
+            encounter_message = String::from("You walked left and encountered an enemy");
         }
         "s" => {
             player.position = (player.position.0, player.position.1 - 1);
-            println!("You walked backwards {}", enemy_found[enemy_chance])
+            println!("You walked backwards {}", encounter_message);
+            encounter_message = String::from("You walked backwards and encountered an enemy");
         }
         _ => {
             player.position = (player.position.0 + 1, player.position.1);
-            println!("You walked right {}", enemy_found[enemy_chance])
+            println!("You walked right {}", encounter_message);
+            encounter_message = String::from("You walked right and encountered an enemy");
         }
     }
-    // Initalize fight with enemy
-    if enemy_chance == 0 {
-        enemy_encounter(player);
+
+    // Initalize fight with enemy if found
+    if enemy_found {
+        enemy_encounter(player, encounter_message);
     }
 }
 
-// Fight system
-fn enemy_encounter(player: &mut Player) {
+// Options that come up to start a fight then returns or calls fight function
+fn enemy_encounter(player: &mut Player, encounter_message: String) {
     loop {
         println!("1. Fight  2. Flee");
-        let input = get_input();
+        let input: String = get_input();
         if input == "1" {
             fight(player, String::from("Slime"));
             break;
         }
         if input == "2" {
-            let flee_success = flee(None);
+            let flee_success: bool = flee(None);
             if flee_success {
                 println!("You successfully fled the fight");
                 break;
             } else {
                 println!("You failed to flee the fight");
+                get_input();
                 fight(player, String::from("Slime"));
                 break;
             }
         }
+        clearscreen::clear().unwrap();
+        println!("{}", encounter_message);
     }
 }
 
-fn fight(player: &mut Player, enemy_name: String) {
-    let mut health: i32 = player.health as i32; // Remove this and clone the player instead
-    let mut player_turn: bool = true;
-    let mut enemy = Enemy {
+fn fight(player_original: &mut Player, enemy_name: String) {
+    // Create enemy and player instance and set turn to one with highest speed
+    let mut player: Player = player_original.clone();
+    let mut enemy: Enemy = Enemy {
         name: enemy_name,
         health: 50,
         defence: 0,
-        damage: 5,
+        damage: 15,
         speed: 50,
     };
+    let mut player_turn: bool = if player.speed > enemy.speed {
+        true
+    } else {
+        false
+    };
 
-    // The fight
+    // Fight loop
     loop {
+        // Check for deaths
         if enemy.health <= 0 {
             println!("Enemy died");
             break;
@@ -202,63 +219,81 @@ fn fight(player: &mut Player, enemy_name: String) {
             println!("You died");
             break;
         }
+
+        // Player turn
         if player_turn {
             player_turn = false;
             loop {
+                // Loop until valid input is entered then process input
                 println!(
                     "Health: {}    {} Health: {}",
                     player.health, enemy.name, enemy.health
                 );
                 println!("1. Attack 2. Magic");
                 println!("3. Item   4. Flee");
-                let input = get_input();
+                let input: String = get_input();
                 match input.as_str() {
                     "1" => {
-                        attack(player, &mut enemy, true);
+                        attack(&mut player, &mut enemy, true);
+                        get_input();
                         break;
                     }
                     "2" => {
                         println!("You don't know any magic dumbass");
+                        get_input();
                         break;
                     }
                     "3" => {
-                        println!("figfsdfsdfsdfht");
+                        println!("Inventory here");
+                        get_input();
                         break;
                     }
                     "4" => {
                         if flee(None) {
                             println!("You successfully fled from {}!", enemy.name);
+                            return;
                         } else {
                             println!("You failed to flee from {}", enemy.name);
                         }
+                        get_input();
                         break;
                     }
                     _ => {}
                 }
             }
-        } else {
+        }
+        // Enemy turn
+        else {
             player_turn = true;
-            let input = get_input();
-            println!("The enemy did 5 damage");
+            attack(&mut player, &mut enemy, false);
             get_input();
         }
     }
 }
 
-fn attack(player: &mut Player, enemy: &mut Enemy, player_attack: bool) -> String {
-    let mut attack_result = String::new();
+// Used in fight to do regular damage to player and enemies
+fn attack(player: &mut Player, enemy: &mut Enemy, player_attack: bool) {
+    // Player attack
     if player_attack {
         let mut damage: i32 = player.damage - enemy.defence;
         if damage < 0 {
             damage = 0;
         }
         enemy.health -= damage;
-        attack_result += "Enemy health: ";
-        attack_result += &enemy.health.to_string();
+        println!("You did {} damage", damage);
     }
-    return attack_result;
+    // Enemy attack
+    else {
+        let mut damage: i32 = enemy.damage - player.defence;
+        if damage < 0 {
+            damage = 0;
+        }
+        player.health -= damage;
+        println!("Enemy did {} damage", damage);
+    }
 }
 
+// Used in enemy encounter and fight
 fn flee(chance_modifier: Option<u32>) -> bool {
     return rand::thread_rng().gen_range(0..FLEE_CHANCE * chance_modifier.unwrap_or(1)) == 0;
 }
